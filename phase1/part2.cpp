3aa2b580306a63c9ca54dc486b2c6336b3b5bcde
Shreyas Katdare <shreyas.08kat@gmail.com>
2023-11-26 14:23:17 +0530

    1.2 cancellation not working...

--- phase1/part2.cpp
+++ phase1/part2.cpp
@@ -1,4 +1,64 @@
 #include "../stl/algorithm"
+// TODO : remove traded offers form alive_buy and alive_sell
+
+bool isValid(std::unordered_map<std::string, long long> alive_buy,
+			std::unordered_map<std::string, long long> alive_sell,
+			std::string order_name, long long amount, char buy_sell )
+{	if(!alive_buy.contains(order_name))
+		alive_buy[order_name] = INVALID;
+
+	if (!alive_sell.contains(order_name))
+		alive_sell[order_name] = INVALID;
+	
+	if (buy_sell == 'b')
+	{
+		if (alive_buy[order_name] == INVALID && alive_sell[order_name] == INVALID)
+		{	
+			return true;
+		}
+		else if (alive_buy[order_name] != INVALID && alive_buy[order_name] >= amount)
+		{	
+			return false;
+		}
+		else if (alive_sell[order_name] == amount)
+		{
+			// TODO : Remove alive buy/sell offer from lin_combs
+			alive_sell[order_name] = INVALID;
+			alive_buy[order_name] = INVALID;
+		
+			return false;
+		}
+		else {
+			// TODO : Remove alive buy offer from lin_combs
+			alive_buy[order_name] = INVALID;
+			return true;
+		}
+		 
+	}
+	else{
+		if (alive_sell[order_name] == INVALID && alive_buy[order_name] == INVALID)
+		{	
+			return true;
+		}
+		else if (alive_sell[order_name] != INVALID && alive_sell[order_name] <= amount)
+		{	
+			return false;
+		}
+		else if (alive_buy[order_name] == amount)
+		{
+			// TODO : Remove alive buy/sell offer from lin_combs
+			alive_sell[order_name] = INVALID;
+			alive_buy[order_name] = INVALID;
+		
+			return false;
+		}
+		else {
+			// TODO : Remove alive buy offer from lin_combs
+			alive_sell[order_name] = INVALID;
+			return true;
+		}
+	}
+}
 
 void powerset_helper(std::vector<int> &v, int i, std::vector<int> &subset,
 		     std::vector<std::vector<int>> &powerset)
@@ -81,7 +141,9 @@ bool arbitrage(
 
 void part_2_sol(std::string &message, std::string &sp, int &num_alive,
         std::vector<std::unordered_map<std::string, long long>> &lin_combs,
-        std::vector<std::pair<long long, char>> &prices, int &profit)
+        std::vector<std::pair<long long, char>> &prices, int &profit,
+		std::unordered_map<std::string, long long> alive_buy,
+		std::unordered_map<std::string, long long> alive_sell)
 {
 	int begin = 0;
 	while (true) {
@@ -103,7 +165,9 @@ void part_2_sol(std::string &message, std::string &sp, int &num_alive,
 		long long   amount;
 		char        buy_sell;
 		int         idx = -1;
-		
+		std::vector<std::string> order_stock_names;
+		std::string order_name;
+
 		// Get linear combination
 		DEBUG_MSG("Alive: " << num_alive << '\n');
 		DEBUG_MSG("Stock\tQuantity\n");
@@ -125,12 +189,20 @@ void part_2_sol(std::string &message, std::string &sp, int &num_alive,
 				lin_combs.resize(2 * lin_combs.size());
 				prices.resize(2 * prices.size());
 			}
-
+			order_stock_names.push_back(stock_name);
 			lin_combs[num_alive][stock_name] = quantity;
 
 			DEBUG_MSG(stock_name << '\t' << quantity << '\n');
 		} while (curr[idx + 1] - '9' > 0);
 		
+		// Sorting Stock names in alphabetical order
+		std::sort(order_stock_names.begin(), order_stock_names.end());
+		
+		// Storing Sorted order/line name
+		for (const std::string& w : order_stock_names) {
+        	order_name += w + " ";
+    	}
+
 		// Get price
 		while (curr[++idx] != ' ')
 			amount_str += curr[idx];
@@ -143,38 +215,52 @@ void part_2_sol(std::string &message, std::string &sp, int &num_alive,
 		
 		prices[num_alive] = std::make_pair(amount, buy_sell);
 
-		std::vector<std::pair<std::vector<int>, int>> arbitrages;
-		if (arbitrage(num_alive, lin_combs, prices, arbitrages)) {
-			int idx =
-			        std::max_element(arbitrages.begin(),
-			                         arbitrages.end(),
-			                         [](const auto &l, const auto &r)
-			                         { return l.second < r.second; })
-			            - arbitrages.begin();
-			for (int i = arbitrages[idx].first.size() - 1; i >= 0; i--) {
-				// Print traded orders
-				for (auto it = lin_combs[arbitrages[idx].first[i]].begin();
-				     it != lin_combs[arbitrages[idx].first[i]].end(); it++) {
-					DEBUG_MSG(it->first << ' ' << it->second << ' ');
-					std::cout << it->first << ' ' << it->second << ' ';
-				}
-				DEBUG_MSG(prices[i].first << ' ');
-				DEBUG_MSG(char('b' + 's' - prices[i].second) << "#\n");
-				std::cout << prices[i].first << ' ';
-				std::cout << char('b' + 's' - prices[i].second) << "#\n";
-
-				// Remove traded orders
-				lin_combs.erase(lin_combs.begin() + arbitrages[idx].first[i]);
-				prices.erase(prices.begin() + arbitrages[idx].first[i]);
-				num_alive--;
-			}
-			DEBUG_MSG(profit << '\n');
-			profit += arbitrages[idx].second;
-		} else {
-			DEBUG_MSG("No Trade\n");
+		// Check for cancellation
+		if (!isValid(alive_buy, alive_sell, order_name, amount, buy_sell))
+		{	DEBUG_MSG("No Trade\n");
 			std::cout << "No Trade\n";
+			// TODO : Remove this offer from lincombs and prices and also num_alive--
 		}
+		else{
 
+			std::vector<std::pair<std::vector<int>, int>> arbitrages;
+			if (arbitrage(num_alive, lin_combs, prices, arbitrages)) {
+				int idx =
+						std::max_element(arbitrages.begin(),
+										arbitrages.end(),
+										[](const auto &l, const auto &r)
+										{ return l.second < r.second; })
+							- arbitrages.begin();
+				for (int i = arbitrages[idx].first.size() - 1; i >= 0; i--) {
+					// Print traded orders
+					for (auto it = lin_combs[arbitrages[idx].first[i]].begin();
+						it != lin_combs[arbitrages[idx].first[i]].end(); it++) {
+						DEBUG_MSG(it->first << ' ' << it->second << ' ');
+						std::cout << it->first << ' ' << it->second << ' ';
+					}
+					DEBUG_MSG(prices[i].first << ' ');
+					DEBUG_MSG(char('b' + 's' - prices[i].second) << "#\n");
+					std::cout << prices[i].first << ' ';
+					std::cout << char('b' + 's' - prices[i].second) << "#\n";
+
+					// Remove traded orders
+					lin_combs.erase(lin_combs.begin() + arbitrages[idx].first[i]);
+					prices.erase(prices.begin() + arbitrages[idx].first[i]);
+					num_alive--;
+				}
+				DEBUG_MSG(profit << '\n');
+				profit += arbitrages[idx].second;
+			} else {
+				DEBUG_MSG("No Trade\n");
+				std::cout << "No Trade\n";
+		
+				// No Arbitrage found so this is alive order
+				if (buy_sell == 'b')
+					alive_buy[order_name] = amount;
+				else
+					alive_sell[order_name] = amount;
+			}
+		}
 		num_alive++;
 		begin = end + 2;
 		if (begin > message.size() - 1)
@@ -192,10 +278,12 @@ void part_2(Receiver rcv)
 	std::vector<std::pair<long long, char>>                 prices(64);
 	int num_alive = 0;
 	int profit    = 0;
+	std::unordered_map<std::string, long long> alive_buy;
+	std::unordered_map<std::string, long long> alive_sell;
 
 	while (!input_end) {
 		std::string message = rcv.readIML();
-		part_2_sol(message, sp, num_alive, lin_combs, prices, profit);
+		part_2_sol(message, sp, num_alive, lin_combs, prices, profit, alive_buy, alive_sell);
 
 		if (message.find('$') != std::string::npos) {
 			rcv.terminate();
