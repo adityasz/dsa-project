6407558bb961c281cf1320cc2b939e09eea3d11b
Aditya Singh <1adityasingh@proton.me>
2023-11-28 14:29:30 +0530

    Update trader.cpp, blank_trader.cpp

--- phase2/trader.cpp
+++ phase2/trader.cpp
@@ -1,13 +1,38 @@
 // This is your trader. Place your orders from here
 
 #include <string>
+#include <iostream>
+#include <atomic>
+
+extern std::atomic<int> commonTimer;
 
 int reader(int time)
 {
+	std::cout << time << "Harry SELL AMD $1 #32 1" << std::endl;
 	return 1;
 }
 
 int trader(std::string *message)
 {
 	return 1;
+}
+
+void* userThread(void *arg)
+{
+	int thread_id = *(int *) arg;
+	while (true) {
+		int currentTime;
+		{
+			currentTime = commonTimer.load();
+		}
+		int end = reader(currentTime);
+		if (end)
+			break;
+	}
+	return nullptr;
+}
+
+void* userTrader(void *arg)
+{
+	return nullptr;
 }
